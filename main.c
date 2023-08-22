20c145a8c548ff387a038786723908afd6487bde
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-22 17:05:53 +0100

    main file

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "monty.h"
+
+/**
+ * main - Start the program
+ * @argc: Argument count
+ * @argv: Argument Value(s)
+ *
+ * Return: Error or success code
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Check if valid file
+	// Open and read file here
+	// read each line
+	// Add each line to a stack
+	// Pop and interpret
+	exit(1);
+}
