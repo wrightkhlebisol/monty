f6569d04ad227099eb8a629c48b3748b3358b7fc
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-23 19:28:38 +0100

    struct of function pointers

--- main.c
+++ main.c
@@ -10,8 +10,17 @@
 int main(int argc, char **argv)
 {
 	FILE *fp = NULL;
-	int MAX_SIZE = 128;
+	int MAX_SIZE = 128, lineht = 0;
 	char chunk[MAX_SIZE];
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pop", pop},
+/*		{"pall", pall},
+		{"pint", pint},
+		{"swap", swap},
+		{"add", add},
+*/		NULL
+	};
 
 	if (argc != 2)
 	{
@@ -29,11 +38,22 @@ int main(int argc, char **argv)
 	while(fgets(chunk, MAX_SIZE, fp) != NULL)
 	{
 		char *token_s = strtok(chunk, "\t\r\n ");
-		while(token_s)
+		int i = 0;
+
+		while (opcodes[i].opcode != NULL )
 		{
-			printf("%s\n", token_s);
-			token_s = strtok(NULL, "\t\r\n ");
+			if (strcmp(token_s, opcodes[i].opcode) == 0)
+			{
+				opcodes[i].f(head, lineht);
+			}
+			i++;
 		}
+
+		
+		token_s = strtok(NULL, "\t\r\n ");
+		printf("%s\n", token_s);
+		
+		lineht++;
 		// Add each line to a stack
 	}
 	fclose(fp);
