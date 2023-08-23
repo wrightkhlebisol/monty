097640db168d572b754290742c0548be80953681
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-23 17:04:59 +0100

    add the printing to standard error with name of file to main file

--- main.c
+++ main.c
@@ -21,7 +21,10 @@ int main(int argc, char **argv)
 
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
+	}
 
 	while(fgets(chunk, MAX_SIZE, fp) != NULL)
 	{
