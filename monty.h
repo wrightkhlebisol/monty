21a7c40c7fdc1cc5d6d8f430a7757f36979fb183
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-23 16:02:22 +0100

    include string

--- monty.h
+++ monty.h
@@ -2,6 +2,7 @@
 #define _STACKS_QUEUES_
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
