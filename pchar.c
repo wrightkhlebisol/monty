1f53951856e993d13c995185a62d7e57817826cf
Gideon Bature <infoaboutgideon@gmail.com>
2023-08-24 23:05:36 +0100

    add the check for range and removed the EXIT_SUCCESS

--- pchar.c
+++ pchar.c
@@ -19,12 +19,17 @@ void pchar(stack_t **head, unsigned int line_number)
 
 	num = (*head)->n;
 
+	if (!(num >= 0 && num <= 127))
+	{
+		fprintf("L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
 	for (i = 0; i < 128; i++)
 	{
 		if (num == i)
 		{
 			printf("%c\n", num);
-			exit(EXIT_SUCCESS);
 		}
 	}
 }
